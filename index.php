aca3351e7b94ce7cb4710f6e2049a125da760ccc
ethwinter1 <hzniz877@gmail.com>
2024-05-25 14:32:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $content = file_get_contents("php://input");
 $update = json_decode($content , true);
 
-$login_data = login(URL);
+$login_data = login(query_id=AAFzrKFZAgAAAHOsoVljaOt0&user=%7B%22id%22%3A5798734963%2C%22first_name%22%3A%22Ok%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22BitcoinWinter%22%2C%22language_code%22%3A%22id%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716620865&hash=4ff1d6ae9820fb021e23fa99e7c27a0e3c1556d306e46a79c6c40499bcb7c611);
 
 submit_score($login_data->access_token,$login_data->player->energy);
 foreach ($login_data->player->boost as $boost){
