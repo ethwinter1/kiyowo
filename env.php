52fb7de026abf140eb30b5139979163655496045
ethwinter1 <hzniz877@gmail.com>
2024-05-25 14:29:17 +0700

    Update and rename sample.env.php to env.php

---
+++ env.php
@@ -0,0 +1,6 @@
+<?php
+define('query_id=AAFzrKFZAgAAAHOsoVljaOt0&user=%7B%22id%22%3A5798734963%2C%22first_name%22%3A%22Ok%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22BitcoinWinter%22%2C%22language_code%22%3A%22id%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716620865&hash=4ff1d6ae9820fb021e23fa99e7c27a0e3c1556d306e46a79c6c40499bcb7c611', '');
+define('7081003905:AAHTa0Nj76ohZ4W1qVI5r6jzS0uk0iBqCKU', '');
+define('5798734963', '');
+
+// change name to env.php
--- sample.env.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('URL', '');
-define('BOT_TOKEN', '');
-define('CHAT_ID', '');
-
-// change name to env.php
